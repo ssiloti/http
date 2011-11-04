607e18d03b45dff8cee4394d8e26a8a1382a1a3a
Steven Siloti <ssiloti@gmail.com>
2011-11-04 11:26:36 -0700

    Add include guards

--- http/asterisk.hpp
+++ http/asterisk.hpp
@@ -7,6 +7,9 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+#ifndef HTTP_ASTERISK_HPP
+#define HTTP_ASTERISK_HPP
+
 namespace http {
 
 struct asterisk
@@ -16,3 +19,5 @@ struct asterisk
 };
 
 }
+
+#endif
